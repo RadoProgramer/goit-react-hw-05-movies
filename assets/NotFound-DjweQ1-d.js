f9e34135c6d3a2b70e6154422efbc90e1a2a7d0c
RadoProgramer <145141434+RadoProgramer@users.noreply.github.com>
2024-06-16 08:20:07 +0100

    Updates

--- assets/NotFound-DjweQ1-d.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-DjCAYS1Q.js";function n(){return o.jsxs("div",{className:"not-found",children:[o.jsx("h1",{children:"404 - Not Found"}),o.jsx("p",{children:"The page you are looking for does not exist."})]})}export{n as default};
